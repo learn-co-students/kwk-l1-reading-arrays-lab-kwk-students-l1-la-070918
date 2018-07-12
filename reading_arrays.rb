c32af552bc938c49c710db81cd24bad835441318
elizabethlazear <elizabethlazear04@gmail.com>
2018-07-12 17:07:39 +0000

    Done.

--- reading_arrays.rb
+++ reading_arrays.rb
@@ -1,4 +1,4 @@
-STUDENT_NAMES = [
+student_names = [
   "Adele",
   "Beyoncé",
   "Cardi B",
@@ -7,26 +7,49 @@ STUDENT_NAMES = [
   "Rihanna"
 ]
 
-def first_student_by_index
-  # Write a solution that returns the first student in the array student_names
-end
 
-def fourth_student_by_index
-  # Write a solution that returns the first student in the array student_names
-end
 
-def last_student_by_index
-  # Write a solution that returns the first student in the array student_names
-end
 
-def first_student_by_method
-  # Write a solution that returns the first student using the built in .first method
+def first_student_by_index(student_names)
+puts student_names[0]
 end
+first_student_by_index(student_names)
+ 
 
-def last_student_by_method
-  # Write a solution that returns the first student using the built in .last method
-end
 
-def first_second_and_third_students
-  # Write a solution that returns the first, second and third students
+def fourth_student_by_index(student_names)
+  puts student_names[3]
 end
+  fourth_student_by_index(student_names)
+
+
+
+def last_student_by_index(student_names)
+puts student_names [5]
+end
+last_student_by_index(student_names)
+ 
+ 
+
+def first_student_by_method(student_names)
+  puts student_names.first
+end
+first_student_by_method(student_names)
+
+  
+
+def last_student_by_method(student_names)
+  puts student_names.last
+end
+last_student_by_method(student_names)
+  
+
+
+
+def first_second_and_third_students(student_names)
+  puts student_names.first
+  puts student_names[1...3]
+end
+first_second_and_third_students(student_names)
+  
+  
